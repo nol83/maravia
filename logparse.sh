3b25e27cce2a619b28abd28ad1b298caaa6ff0dc
Nick Olson <nicholas.olson83@gmail.com>
2012-06-28 16:19:07 -0500

    Revision 1

--- logparse.sh
+++ logparse.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-log=/var/log/syslog
+dir="/srv/rsyslog/"
+log="syslog"
 
-while getopts ":s:" opt; do
-
-case $opt in
-    s)
-      #echo "-a was triggered, Parameter: $OPTARG"
-      grep -E "$OPTARG" $log
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG"
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument."
-      exit 1
-      ;;
-  esac
+while getopts ":q:d:l:" opt;
+do
+   case $opt in
+      q)
+         query=$OPTARG
+         ;;
+      d)
+         dir=$OPTARG
+         ;; 
+      l)
+         log=$OPTARG
+         ;;
+      \?)
+         echo "Invalid option: -$OPTARG"
+         exit 1
+         ;;
+      :)
+         echo "Option -$OPTARG requires an argument."
+         exit 1
+         ;;
+   esac
 done
+
+echo "          Query - \"$query\""
+echo "  TimeStamp     |       Server"
+echo "-----------------------------------------"
+find $dir -type f -name $log -print0 | while read -d $'\0' file
+do
+   grep --color "$query" $file | awk '{print $1,$2,$3,"|",$4}'
+done
+exit 0
